03110b96f6073e1b998eb1d9d52de43c7bc629ec
PeterDiyo <princediyo93@gmail.com>
2026-01-27 09:41:24 +0200

    #12 IDE Layout

--- src/features/projects/hooks/use-projects.ts
+++ src/features/projects/hooks/use-projects.ts
@@ -8,6 +8,10 @@ export const useProjects = () => {
   return useQuery(api.projects.get);
 };
 
+export const useProject = (projectId: Id<"projects">) => {
+  return useQuery(api.projects.getById, { id: projectId });
+};
+
 export const useProjectsPartial = (limit: number) => {
   return useQuery(api.projects.getPartial, { limit });
 };
@@ -31,6 +35,44 @@ export const useCreateProject = () => {
           newProject,
           ...existingProjects,
         ]);
+      }
+    },
+  );
+};
+
+export const useRenameProject = (projectId: Id<"projects">) => {
+  return useMutation(api.projects.rename).withOptimisticUpdate(
+    (localStore, args) => {
+      const existingProject = localStore.getQuery(api.projects.getById, {
+        id: projectId,
+      });
+
+      if (existingProject !== undefined && existingProject !== null) {
+        localStore.setQuery(
+          api.projects.getById,
+          { id: projectId },
+          {
+            ...existingProject,
+            name: args.name,
+            updatedAt: Date.now(),
+          },
+        );
+      }
+      const existingProjects = localStore.getQuery(api.projects.get);
+
+      if (existingProjects !== undefined) {
+        localStore.setQuery(
+          api.projects.get,
+          {},
+          existingProjects.map((project) => {
+            if (project._id === projectId) {
+              return project._id === args.id
+                ? { ...project, name: args.name, updatedAt: Date.now() }
+                : project;
+            }
+            return project;
+          }),
+        );
       }
     },
   );
